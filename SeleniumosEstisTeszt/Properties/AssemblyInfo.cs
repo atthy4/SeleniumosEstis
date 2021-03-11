4e5d29a4cc4a8c961db2198e7c43e2fa7f8db135
atthy4 <74587739+atthy4@users.noreply.github.com>
2021-03-11 19:16:55 +0100

    asd
    asd

---
+++ SeleniumosEstisTeszt/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumosEstisTeszt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumosEstisTeszt")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9bb7b89a-7513-48db-a332-e0927c17a765")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumosEstisTeszt/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumosEstisTeszt
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver dw = new FirefoxDriver();
+            dw.Manage().Window.Maximize();
+            dw.Navigate().GoToUrl("https://www.portfolio.hu/");
+            string s = dw.Url;
+            Assert.AreEqual("tukorforegep", s);
+        }
+    }
+}
